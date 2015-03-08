0b17b399249729e676f164738a3eff48e4adebe2
Edward Kmett <ekmett@gmail.com>
2015-03-08 05:54:32 -0400

    copyright bump

--- src/Data/LCA/Online.hs
+++ src/Data/LCA/Online.hs
@@ -2,7 +2,7 @@
 -----------------------------------------------------------------------------
 -- |
 -- Module      :  Data.LCA.Online
--- Copyright   :  (C) 2011-2012 Edward Kmett
+-- Copyright   :  (C) 2011-2015 Edward Kmett
 -- License     :  BSD-style (see the file LICENSE)
 --
 -- Maintainer  :  Edward Kmett <ekmett@gmail.com>
--- src/Data/LCA/View.hs
+++ src/Data/LCA/View.hs
@@ -2,7 +2,7 @@
 -----------------------------------------------------------------------------
 -- |
 -- Module      :  Data.LCA.View
--- Copyright   :  (C) 2012 Edward Kmett
+-- Copyright   :  (C) 2012-2015 Edward Kmett
 -- License     :  BSD-style (see the file LICENSE)
 -- Maintainer  :  Edward Kmett <ekmett@gmail.com>
 -- Stability   :  provisional
